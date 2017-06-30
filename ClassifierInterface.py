ee637e838228467d8e4aa91ce927f25009531fc5
Mahmoud Elagdar <ma7mouda7mad@gmail.com>
2017-06-30 21:38:30 +0200

    fix mix of spaces and tabs

--- ClassifierInterface.py
+++ ClassifierInterface.py
@@ -2,11 +2,11 @@ from queue import Queue
 import json
 
 class Tweet():
-        '''
-        Stores main data from a json tweet received by the Twitter Streaming API
-        '''
+	'''
+	Stores main data from a json tweet received by the Twitter Streaming API
+	'''
 	def __init__(self, tweet):
-                '''tweet: tweet json string from the Twitter Streaming API'''
+		'''tweet: tweet json string from the Twitter Streaming API'''
 		tweet_dict=json.loads(tweet)
 		self.id = tweet_dict['id']
 		self.text = tweet_dict['text']
@@ -16,27 +16,27 @@ class Tweet():
 		self.sentiment = None
 
 class PseudoTweet():
-        '''
-        Stores a pseudo tweet. for using normal text as a Tweet
-        '''
+	'''
+	Stores a pseudo tweet. for using normal text as a Tweet
+	'''
 	def __init__(self, tweet):
-                '''tweet: list of form [tweet_id, tweet_text]'''
+		'''tweet: list of form [tweet_id, tweet_text]'''
 		self.id = tweet[0]
 		self.text = tweet[1]
 		self.sentiment = None
 	
 class IncomingQueue():
-        '''
-        Receives tweet objects and adds them to a queue for processing
-        '''
+	'''
+	Receives tweet objects and adds them to a queue for processing
+	'''
 	def __init__(self):
 		self.queue = Queue()
 
 	def put(self, tweet):
-                '''
-                Extracts required data from a tweet json string,
-                then adds the generated object to a queue
-                '''
+		'''
+		Extracts required data from a tweet json string,
+		then adds the generated object to a queue
+		'''
 		if len(tweet)==1: self.queue.put(Tweet(tweet))
 		## case of a pseudo tweet
 		else: self.queue.put(PseudoTweet(tweet))
@@ -45,24 +45,24 @@ class IncomingQueue():
 	def get(self): return self.queue.get()
 	
 class ClassifierInterface():
-        '''
-        An interface for classifying tweets from a provided queue using a provided classifier
-        '''
+	'''
+	An interface for classifying tweets from a provided queue using a provided classifier
+	'''
 	def __init__(self, classifier, in_queue):
-                '''
-                classifier: a classifier that can receive a list of tweet texts and return an array of class probabilities
-                in_queue: a queue the classifier can get tweets objects from
-                '''
+		'''
+		classifier: a classifier that can receive a list of tweet texts and return an array of class probabilities
+		in_queue: a queue the classifier can get tweets objects from
+		'''
 		self.classifier = classifier
 		self.in_queue = Queue()
 		self.ready_queue = Queue()
 		
 	def classify_batch(self, batch_size=-1):
-                '''
-                Classifies a batch of tweets from the in_queue and adds them to the ready_queue
-                batch_size: the batch to be extracted, use -1 to get all items from the queue
-                get the classified tweets from the objects's ready_queue
-                '''
+		'''
+		Classifies a batch of tweets from the in_queue and adds them to the ready_queue
+		batch_size: the batch to be extracted, use -1 to get all items from the queue
+		get the classified tweets from the objects's ready_queue
+		'''
 		batch = []
 		batch_size = min(batch_size, in_queue.qsize())
 		if batch_size==-1: batch_size=in_queue.qsize()
